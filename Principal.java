d6e6b97c12577bb031e22ae0e4127c753fbe8d70
Douglas Antonio Monteiro Alcantara Justino <33661367+DougsterS@users.noreply.github.com>
2018-08-13 21:36:56 -0300

    Inicio de período
    Primeiros códigos da cadeira

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package br.cc.unipe.aula01;
+
+import br.cc.unipe.aula01.Conta;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.exibirMensagem();
+	}
+
+}
