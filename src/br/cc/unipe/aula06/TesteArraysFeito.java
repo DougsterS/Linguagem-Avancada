35869fdd54526bb544c70d52fc9d8fb011030518
Douglas Antonio Monteiro Alcantara Justino <33661367+DougsterS@users.noreply.github.com>
2018-09-11 20:17:32 -0300

    0.6.2
    Adição de comentarios

--- src/br/cc/unipe/aula06/TesteArraysFeito.java
+++ src/br/cc/unipe/aula06/TesteArraysFeito.java
@@ -9,14 +9,14 @@ public class TesteArraysFeito {
 		int[] a = new int[10];
 		
 		//preenche
-		Arrays.fill(a, 10);
+		Arrays.fill(a, 10);//primeira declaracao e a variavel e a segunda o valor que desejamos preencher nos espacos de memoria
 		
 		//Copia
 		int[] copia = Arrays.copyOfRange(a, 0, 10);
 		
 		System.out.println("Array: " + Arrays.toString(a));
 		
-		System.out.println("Cópia: " + Arrays.toString(copia));
+		System.out.println("CÃ³pia: " + Arrays.toString(copia));
 
 	}
 }
